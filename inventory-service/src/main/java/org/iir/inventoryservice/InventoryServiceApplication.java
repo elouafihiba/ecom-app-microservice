84e3d518a7b31bce4ae5b7dae232a2e8df5c0945
hiba el ouafi <elouafihiba1@gmail.com>
2023-12-26 02:06:46 +0000

    p2

--- inventory-service/src/main/java/org/iir/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/iir/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,14 @@
 package org.iir.inventoryservice;
 
+import org.iir.inventoryservice.entities.Product;
+import org.iir.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +16,21 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository){
+		return args -> {
+			Random random=new Random();
+			for (int i = 1; i < 10; i++) {
+				productRepository.saveAll(List.of(
+						Product.builder()
+								.name("Computer"+i)
+								.price(1200+Math.random()*1000)
+								.quantity(1+ random.nextInt(200)).build()
+				));
+			}
+
+		};
+	}
+
 
 }
---
+++ inventory-service/src/main/java/org/iir/inventoryservice/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.iir.inventoryservice.repositories;
+
+import org.iir.inventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
